d42618a480af5006cc86bde941617a9394ca70c6
Marcin <Majsyn@gmail.com>
2018-07-09 07:43:55 +0200

    punkt 1 zadanie-java-31

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+class Car {
+    int year;
+    double capacity;
+    String brand;
+    String model;
+
+    Car(String b, String m, int y, double c){
+        brand = b;
+        model = m;
+        year = y;
+        capacity = c;
+    }
+}
